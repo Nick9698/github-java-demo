a73fa778934e321ef637d5784bba95bd8613f079
Nikhil Tawdare <tawdarenikhil@gmail.com>
2024-04-20 17:52:27 +0530

    Modification for Employee Class

--- src/com/nick/MainApp.java
+++ src/com/nick/MainApp.java
@@ -1,9 +1,14 @@
 package com.nick;
 
+import com.entities.Student;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !!!");
+		System.out.println("Hello World !!!\n");
+		Student s1 = new Student(1645079, "Nikhil");
+		System.out.println(s1.getRollNo());
+		System.out.println(s1.getName());
 	}
 
 }
